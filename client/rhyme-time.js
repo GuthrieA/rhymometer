75773e43e4195454c0b23c9c2964f15200395457
Andrew <andrew.hopkins.guthrie@gmail.com>
2016-04-21 15:26:11 -0400

    Initial commit

---
+++ client/rhyme-time.js
@@ -0,0 +1,9 @@
+// Template.rhymeTime.events({
+// 	$("#rhyme").click(function() {
+// 		$("#output").empty();
+// 		var rhymes = pronouncing.rhymes($("#word").val());
+// 		$.each(rhymes, function(i, item) {
+// 			$("#output").append("<li>" + item + "</li>");
+// 		});
+// 	});
+// });

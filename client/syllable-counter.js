d946ececb938452565093ef050b39a290cacc661
Andrew <andrew.hopkins.guthrie@gmail.com>
2016-04-26 15:33:25 -0400

    Add multiple word syllable counting

--- client/syllable-counter.js
+++ client/syllable-counter.js
@@ -1,17 +1,35 @@
 let pronouncing = require('pronouncing/build/pronouncing-browser');
 
 
-let str = "How are you doing today?";
-let punctuationless = str.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"");
-let finalString = punctuationless.replace(/\s{2,}/g," ");
-let arrayOfFinalString = finalString.split(" ");
-console.log(arrayOfFinalString);
-
 Template.syllableCounter.events({
 	'submit .syllable-counter': function(event) {
+		let wordSyllables = 0;
+		let count = 0;
     event.preventDefault();
+
+		console.log ("#phrase");
+		let str = ("phrase");
+		let punctuationless = str.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"");
+		let lowerString = punctuationless.toLowerCase();
+		let finalString = lowerString.replace(/\s{2,}/g," ");
+		let arrayOfFinalString = finalString.split(" ");
+		console.log(arrayOfFinalString);
+
 		$("#output").empty();
-		let syllables = pronouncing.syllableCount(pronouncing.phonesForWord($("#word").val())[0]);
+
+		for (word in arrayOfFinalString){
+		console.log(arrayOfFinalString[word]);
+		wordSyllables = pronouncing.syllableCount(pronouncing.phonesForWord(arrayOfFinalString[word])[0]);
+		console.log(wordSyllables);
+		count += wordSyllables;
+		}
+
+		console.log(count);
+
+
+
+
+		let syllables = pronouncing.syllableCount(pronouncing.phonesForWord($("#phrase").val())[0]);
 			$("#output").append(syllables);
 			console.log(syllables);
 	}
